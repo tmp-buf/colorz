dcf670fa8d8724328d9a01ae5e91377306303a3c
tmp-buf <>
2022-01-08 01:56:47 -0300

    Move color name check code to `colorCodeOf` function

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const testing = std.testing;
 const assert = std.debug.assert;
 
 /// Format `fmt` with the corresponding ANSI color codes.
@@ -14,11 +13,10 @@ const assert = std.debug.assert;
 /// - `<yellow>`
 /// - `<b>`: Bold mode
 /// - `<d>`: Dim mode
-/// - `<r>`, `</r>`: Reset
+/// - `<r>`: Reset
 pub fn colorFormat(comptime fmt: []const u8) []const u8 {
     comptime var new_fmt: [fmt.len * 4]u8 = undefined;
     comptime var new_fmt_i: usize = 0;
-    const ED = "\x1b[";
 
     // TODO: Stress test to see if the line below is necessary
     @setEvalBranchQuota(9999);
@@ -38,33 +36,7 @@ pub fn colorFormat(comptime fmt: []const u8) []const u8 {
                 while (i < fmt.len and fmt[i] != '>') : (i += 1) {}
 
                 const color_name = fmt[start..i];
-                const color_str = color_picker: {
-                    if (strEql(color_name, "black")) {
-                        break :color_picker ED ++ "30m";
-                    } else if (strEql(color_name, "blue")) {
-                        break :color_picker ED ++ "34m";
-                    } else if (strEql(color_name, "b")) {
-                        break :color_picker ED ++ "1m";
-                    } else if (strEql(color_name, "d")) {
-                        break :color_picker ED ++ "2m";
-                    } else if (strEql(color_name, "cyan")) {
-                        break :color_picker ED ++ "36m";
-                    } else if (strEql(color_name, "green")) {
-                        break :color_picker ED ++ "32m";
-                    } else if (strEql(color_name, "magenta")) {
-                        break :color_picker ED ++ "35m";
-                    } else if (strEql(color_name, "red")) {
-                        break :color_picker ED ++ "31m";
-                    } else if (strEql(color_name, "white")) {
-                        break :color_picker ED ++ "37m";
-                    } else if (strEql(color_name, "yellow")) {
-                        break :color_picker ED ++ "33m";
-                    } else if (strEql(color_name, "r")) {
-                        break :color_picker ED ++ "0m";
-                    } else {
-                        @compileError("Invalid color name: " ++ color_name);
-                    }
-                };
+                const color_str = colorCodeOf(color_name);
                 var orig = new_fmt_i;
                 new_fmt_i += color_str.len;
                 std.mem.copy(u8, new_fmt[orig..new_fmt_i], color_str);
@@ -81,22 +53,41 @@ pub fn colorFormat(comptime fmt: []const u8) []const u8 {
     return new_fmt[0..new_fmt_i];
 }
 
-/// Test for equality between `s1` and `s2`.
+/// Return corresponding ANSI color code for the comptime string `s`
+fn colorCodeOf(comptime s: []const u8) []const u8 {
+    const ED = "\x1b[";
+    const encoded = col: {
+        if (strEql(s, "black")) {
+            break :col ED ++ "30m";
+        } else if (strEql(s, "blue")) {
+            break :col ED ++ "34m";
+        } else if (strEql(s, "b")) {
+            break :col ED ++ "1m";
+        } else if (strEql(s, "d")) {
+            break :col ED ++ "2m";
+        } else if (strEql(s, "cyan")) {
+            break :col ED ++ "36m";
+        } else if (strEql(s, "green")) {
+            break :col ED ++ "32m";
+        } else if (strEql(s, "magenta")) {
+            break :col ED ++ "35m";
+        } else if (strEql(s, "red")) {
+            break :col ED ++ "31m";
+        } else if (strEql(s, "white")) {
+            break :col ED ++ "37m";
+        } else if (strEql(s, "yellow")) {
+            break :col ED ++ "33m";
+        } else if (strEql(s, "r")) {
+            break :col ED ++ "0m";
+        } else {
+            @compileError("Invalid color name: " ++ s);
+        }
+    };
+
+    return encoded;
+}
+
+/// Test for equality between two strings `s1` and `s2`.
 fn strEql(s1: []const u8, s2: []const u8) bool {
     return std.mem.eql(u8, s1, s2);
-}
-
-test "strEql function" {
-    const s1 = "function";
-    const s2 = s1;
-    try testing.expect(strEql(s1, s2) == true);
-    try testing.expect(strEql(s1, "Function") == false);
-}
-
-test "colorFormat function" {
-    const s1 = "<red>Red<r>";
-    const s2 = "\x1b[31mRed\x1b[0m";
-    assert(strEql(colorFormat(s1), s2));
-
-    std.debug.print("{s}\n", .{colorFormat("<green><b>[OK]<r> Color test <b>passed!<r>")});
 }
