4c3ccd3eb0ca7bca4ba7772ce4bc5a8416b310e6
tmp-buf <>
2022-01-09 02:37:51 -0300

    Replace debug.assert() with testing.expect() in tests

--- src/tests.zig
+++ src/tests.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 const print = std.debug.print;
-const assert = std.debug.assert;
+const expect = std.testing.expect;
 
 const colorfmt = @import("main.zig");
 
 test "Formatting with colorFormat function" {
     const s1 = "<red>Red<r>";
     const s2 = "\x1b[31mRed\x1b[0m";
-    assert(std.mem.eql(u8, colorfmt.colorFormat(s1), s2));
+    try expect(std.mem.eql(u8, colorfmt.colorFormat(s1), s2) == true);
 
     print("{s}\n", .{colorfmt.colorFormat("<green><b>[OK]<r>")});
 }
@@ -16,7 +16,7 @@ test "Formatting with Palette struct" {
     const p = colorfmt.Palette;
     const s1 = "\x1b[32m" ++ "\x1b[1m" ++ "[OK]" ++ "\x1b[0m";
     const s2 = p.green ++ p.bold ++ "[OK]" ++ p.reset;
-    assert(std.mem.eql(u8, s1, s2));
+    try expect(std.mem.eql(u8, s1, s2) == true);
     print("{s}\n", .{s1});
 
 }
