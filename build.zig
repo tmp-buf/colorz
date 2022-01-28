b248a7598528278af8f69d59579cba504a6da547
tmp-buf <>
2022-01-28 01:54:13 -0300

    Add support for different targets in build.zig

--- build.zig
+++ build.zig
@@ -1,16 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    // Optional user selected target platform and optimizations
+    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
     const lib = b.addStaticLibrary("colorz", "src/main.zig");
+    lib.setTarget(target);
     lib.setBuildMode(mode);
     lib.install();
 
     const tests = b.addTest("src/tests.zig");
     tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&tests.step);
 
     const example = b.addExecutable("colors", "src/example.zig");
     example.setBuildMode(mode);
@@ -19,6 +23,4 @@ pub fn build(b: *std.build.Builder) void {
     const example_step = b.step("example", "Run example program");
     example_step.dependOn(&example_run.step);
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&tests.step);
 }
